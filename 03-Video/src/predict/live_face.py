bf6db12bea27b17931025ff1efecc4bc51444ad0
Mohammad Forouhesh <Mohammad.Forouhesh@Outlook.com>
2022-02-05 21:30:21 +0330

    Update live_face.py

--- 03-Video/src/predict/live_face.py
+++ 03-Video/src/predict/live_face.py
@@ -103,49 +103,47 @@ def show_webcam() :
     predictor_landmarks  = dlib.shape_predictor("../../../Models/face_landmarks.dat")
     
     #Lancer la capture video
-    video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture('../../real_data/WIN_20220121_09_37_24_Pro.mp4')
 
     while True:
         # Capture frame-by-frame
         ret, frame = video_capture.read()
-        
         face_index = 0
-        
+
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         rects = face_detect(gray, 1)
         #gray, detected_faces, coord = detect_face(frame)
-
         for (i, rect) in enumerate(rects):
 
             shape = predictor_landmarks(gray, rect)
             shape = face_utils.shape_to_np(shape)
-            
+
             # Identify face coordinates
             (x, y, w, h) = face_utils.rect_to_bb(rect)
             face = gray[y:y+h,x:x+w]
-            
+
             #Zoom on extracted face
             face = zoom(face, (shape_x / face.shape[0],shape_y / face.shape[1]))
-            
+
             #Cast type float
             face = face.astype(np.float32)
-            
+
             #Scale
             face /= float(face.max())
             face = np.reshape(face.flatten(), (1, 48, 48, 1))
-            
+
             #Make Prediction
             prediction = model.predict(face)
             prediction_result = np.argmax(prediction)
-            
+
             # Rectangle around the face
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        
+
             cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-     
+
             for (j, k) in shape:
                 cv2.circle(frame, (j, k), 1, (0, 0, 255), -1)
-            
+
             # 1. Add prediction probabilities
             cv2.putText(frame, "----------------",(40,100 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
             cv2.putText(frame, "Emotional report : Face #" + str(i+1),(40,120 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 0)
@@ -156,7 +154,7 @@ def show_webcam() :
             cv2.putText(frame, "Sad : " + str(round(prediction[0][4],3)),(40,220 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
             cv2.putText(frame, "Surprise : " + str(round(prediction[0][5],3)),(40,240 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
             cv2.putText(frame, "Neutral : " + str(round(prediction[0][6],3)),(40,260 + 180*i), cv2.FONT_HERSHEY_SIMPLEX, 0.5, 155, 1)
-            
+
             # 2. Annotate main image with a label
             if prediction_result == 0 :
                 cv2.putText(frame, "Angry",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
@@ -172,22 +170,22 @@ def show_webcam() :
                 cv2.putText(frame, "Surprise",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             else :
                 cv2.putText(frame, "Neutral",(x+w-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            
+
             # 3. Eye Detection and Blink Count
             leftEye = shape[lStart:lEnd]
             rightEye = shape[rStart:rEnd]
-            
+
             # Compute Eye Aspect Ratio
             leftEAR = eye_aspect_ratio(leftEye)
             rightEAR = eye_aspect_ratio(rightEye)
             ear = (leftEAR + rightEAR) / 2.0
-            
+
             # And plot its contours
             leftEyeHull = cv2.convexHull(leftEye)
             rightEyeHull = cv2.convexHull(rightEye)
             cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
             cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-            
+
             # 4. Detect Nose
             nose = shape[nStart:nEnd]
             noseHull = cv2.convexHull(nose)
@@ -197,12 +195,12 @@ def show_webcam() :
             mouth = shape[mStart:mEnd]
             mouthHull = cv2.convexHull(mouth)
             cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
-            
+
             # 6. Detect Jaw
             jaw = shape[jStart:jEnd]
             jawHull = cv2.convexHull(jaw)
             cv2.drawContours(frame, [jawHull], -1, (0, 255, 0), 1)
-            
+
             # 7. Detect Eyebrows
             ebr = shape[ebrStart:ebrEnd]
             ebrHull = cv2.convexHull(ebr)
@@ -210,7 +208,7 @@ def show_webcam() :
             ebl = shape[eblStart:eblEnd]
             eblHull = cv2.convexHull(ebl)
             cv2.drawContours(frame, [eblHull], -1, (0, 255, 0), 1)
-        
+
         cv2.putText(frame,'Number of Faces : ' + str(len(rects)),(40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, 155, 1)
         cv2.imshow('Video', frame)
         
